f97c0ddf593acc58dcf73676d66774f032de32c4
Andrew A. Chuev <andrew.chuev@gmail.com>
2020-05-09 16:09:11 +0300

    Change project name

--- android/app/src/main/kotlin/com/example/flutterecommerse/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutterecommerse/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutterecommerse
+package com.example.flutterecommerce
 
 import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutterecommerse/main.dart';
+import 'package:flutterecommerce/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
